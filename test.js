c90b83d81ab8df20cadad9cab19b31a8e292c128
Sofia Leon <sofialeon@google.com>
2019-11-01 18:23:31 +0000

    saving for now, still broken

---
+++ test.js
@@ -0,0 +1,8 @@
+/*
+TODO: 
+-data validation test: make sure names has no hyphens or integers and nothing is null (also add data validation)
+-make sure directory is the same order test: create helper function to read directory structure recursively, then create a stack and compare to test stack - (expect.eql())
+	-can create a stack with string path names vs. actual paths
+-test: make sure handlebars compiles where needed (snapshots for this)
+-
+*/
